42edaeba238b300c44c148f738280f6bb5643629
mrpandaxy <vini.r.faria@hotmail.com>
2023-08-11 10:40:09 -0300

    atividade1

---
+++ lista1/atividade1/verificaNumeroPrimo.java
@@ -0,0 +1,12 @@
+package lista1.atividade1;
+
+/**
+ * verificaNumeroPrimo
+ */
+public class verificaNumeroPrimo {
+
+        public static void main(String args[]) {
+            System.out.println("Ola, mundo!");            
+        }
+    
+}
